014100bb98fcd3979c2553fe19a1957cc6053c83
Bl4dSc4n <carlosalbertotuma@yahoo.com.br>
2023-09-08 08:45:53 -0300

    Update l0c4-3xpl0it-CV3.py

--- l0c4-3xpl0it-CV3.py
+++ l0c4-3xpl0it-CV3.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import requests
-import yaml
 import subprocess
 
 def search_module(module_name):
@@ -17,7 +16,8 @@ def search_exploits(query, exploit_source):
     results = []
 
     if exploit_source == "github":
-        search_url = f"https://api.github.com/search/repositories?q={query}"
+        query = query.lower()
+        search_url = f"https://api.github.com/search/repositories?q={query}+in:title+in:readme"
         response = requests.get(search_url)
         data = response.json()
         results.extend(data.get("items", []))
@@ -44,9 +44,9 @@ def main():
         print("Exemplo: python3 script.py laravel")
         sys.exit(1)
     print()
-    module_name = " ".join(sys.argv[1:])
+    module_name = " ".join(sys.argv[1:]).lower()
     cve_result = search_module(module_name)
-    github_query = f"{module_name} exploit"
+    github_query = f"{module_name}+exploit+OR+{module_name}+cve"
 
     if "result" in cve_result and "CVE_Items" in cve_result["result"]:
         cves = cve_result["result"]["CVE_Items"]
@@ -68,18 +68,8 @@ def main():
                 print("URL:", item.get("html_url"))
             print("=" * 80)
 
-    # Search for files containing the specified tag in nuclei-templates
-    output = subprocess.run(["grep", "-iRl", f"tags: *{module_name}*", "../.local/nuclei-templates/"], capture_output=True)
-
-    #print("=" * 80)
-    print("Resultados da pesquisa nos templates Nuclei:")
-    if output.returncode == 0:
-        for line in output.stdout.decode("utf-8").splitlines():
-            print(line)
-        print("=" * 80)
-    else:
-        print("Nenhum resultado encontrado.")
-        print("=" * 80)
+    processo = subprocess.Popen(['python', '3xpl0it-nucl3i.py'], stdin=subprocess.PIPE)
+    processo.communicate(input=module_name.encode())    
 
 if __name__ == "__main__":
     main()
